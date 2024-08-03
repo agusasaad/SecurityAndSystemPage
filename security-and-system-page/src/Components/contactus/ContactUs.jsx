abb6d4109c367b1bc42b73753e8921b0920abcb2
agusasaad <agusasaad1099@hotmail.com>
2024-08-02 21:50:32 -0300

    prueba

--- security-and-system-page/src/Components/contactus/ContactUs.jsx
+++ security-and-system-page/src/Components/contactus/ContactUs.jsx
@@ -4,7 +4,7 @@ import styles from "./ContactUs.module.css";
 const ContactUs = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.ContactUs}></div>
+      <div className={styles.ContactUs}>.</div>
     </div>
   );
 };
