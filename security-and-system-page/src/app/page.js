dadde7395ca297d823b081d0e8d3eca06010c6a2
agusasaad <agusasaad1099@hotmail.com>
2024-08-07 21:18:06 -0300

    se agrega fondo al header y animacion

--- security-and-system-page/src/app/page.js
+++ security-and-system-page/src/app/page.js
@@ -1,21 +1,38 @@
-import SectionOne from "@/Components/sectionone/SectionOne";
+"use client";
+import SectionOne from "./../Components/sectionone/SectionOne";
 import HomePage from "./../Components/homePage/HomePage";
 import NavBar from "./../Components/navbar/NavBar";
 import styles from "./page.module.css";
 import SectionThree from "@/Components/sectionthree/SectionThree";
-import ContactUs from "@/Components/contactus/ContactUs";
+import React, { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 
 export default function Home() {
+  const bgRef = useRef(null);
+
+  useEffect(() => {
+    gsap.set([bgRef.current], {
+      opacity: 0,
+      scale: 0,
+    });
+    gsap.to([bgRef.current], {
+      opacity: 1,
+      scale: 1,
+      ease: "power1",
+      duration: 0.4,
+    });
+  }, []);
+
   return (
     <div className={styles.containerApp}>
       <header className={styles.header}>
         <NavBar />
         <HomePage />
+        <div className={styles.bg} ref={bgRef}></div>
       </header>
       <main className={styles.main}>
         <SectionOne />
         <SectionThree />
-        {/* <ContactUs /> */}
       </main>
     </div>
   );
