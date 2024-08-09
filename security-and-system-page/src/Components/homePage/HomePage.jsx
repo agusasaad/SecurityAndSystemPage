2a537704d83311c9101b26199245a213f184aef0
agusasaad <agusasaad1099@hotmail.com>
2024-08-09 20:12:46 -0300

    se agrega animacion de texto

--- security-and-system-page/src/Components/homePage/HomePage.jsx
+++ security-and-system-page/src/Components/homePage/HomePage.jsx
@@ -6,34 +6,111 @@ import { FiExternalLink } from "react-icons/fi";
 import gsap from "gsap";
 
 const HomePage = () => {
-  const background = useRef(null);
-  const title = useRef(null);
+  const titleRef1 = useRef(null);
+  const titleRef2 = useRef(null);
   const subtitleRef = useRef(null);
   const buttonRef = useRef(null);
 
   useEffect(() => {
-    gsap.set([title.current, subtitleRef.current, buttonRef.current], {
-      y: 100,
-      opacity: 0,
-      visibility: "hidden",
+    const tl = gsap.timeline();
+
+    tl.set(
+      [
+        titleRef1.current,
+        titleRef2.current,
+        subtitleRef.current,
+        buttonRef.current,
+      ],
+      {
+        y: 100,
+        opacity: 0,
+        visibility: "hidden",
+      }
+    );
+
+    tl.to(
+      [
+        titleRef1.current,
+        titleRef2.current,
+        subtitleRef.current,
+        buttonRef.current,
+      ],
+      {
+        opacity: 1,
+        y: 0,
+        visibility: "visible",
+        ease: "power3.out",
+        duration: 0.8,
+        stagger: 0.3,
+      }
+    );
+
+    tl.fromTo(
+      titleRef1.current,
+      { width: "0%", borderRight: "1px solid white", opacity: 0 },
+      {
+        width: "100%",
+        opacity: 1,
+        duration: 1.2,
+        ease: "power2.inOut",
+      },
+      "-=1.5"
+    );
+
+    // Desaparecer el cursor del primer texto justo después de la animación
+    tl.to(
+      titleRef1.current,
+      {
+        borderRight: "none",
+        duration: 0,
+      },
+      "+=0.1"
+    ).fromTo(
+      titleRef2.current,
+      { width: "0%", border: "none" },
+      {
+        width: "100%",
+        borderRight: "2px solid white",
+        duration: 1.2,
+        ease: "power2.inOut",
+      },
+      "-=0.3"
+    );
+
+    tl.to(titleRef2.current, {
+      borderRightColor: "transparent",
+      duration: 0.4,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
     });
-    gsap.to([title.current, subtitleRef.current, buttonRef.current], {
+
+    tl.to(subtitleRef.current, {
       opacity: 1,
       y: 0,
       visibility: "visible",
       ease: "power3.out",
       duration: 0.8,
-      stagger: 0.3,
-      delay: 0.5,
+    });
+
+    tl.to(buttonRef.current, {
+      opacity: 1,
+      y: 0,
+      visibility: "visible",
+      ease: "power3.out",
+      duration: 0.8,
     });
   }, []);
 
   return (
-    <div className={styles.homePage} ref={background}>
+    <div className={styles.homePage}>
       <div className={styles.containerTitle}>
-        <h1 ref={title} className={poppins.className}>
-          Construyendo un Futuro Digital.
-        </h1>
+        <div className={`${styles.typeWritten} ${poppins.className}`}>
+          <h1 ref={titleRef1}>Construyendo</h1>
+        </div>
+        <div className={`${styles.typeWritten} ${poppins.className}`}>
+          <h1 ref={titleRef2}>un Futuro Digital.</h1>
+        </div>
         <div className={styles.containerText}>
           <p className={poppins.className} ref={subtitleRef}>
             Creamos páginas web personalizadas que conectan con tu audiencia y
