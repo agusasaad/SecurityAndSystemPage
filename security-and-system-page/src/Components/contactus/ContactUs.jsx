93f8cf69e2b9e892c8f4390fb7bec3dbd991595c
agusasaad <agusasaad1099@hotmail.com>
2024-08-12 22:32:27 -0300

    se agrega mapa en formato imagen, animacion del puntero y se elimina el mapa de mapbox

--- security-and-system-page/src/Components/contactus/ContactUs.jsx
+++ security-and-system-page/src/Components/contactus/ContactUs.jsx
@@ -1,25 +1,87 @@
 "use client";
 import styles from "./ContactUs.module.css";
-import MapComponent from "./mapComponent/MapComponent";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { poppins } from "@/Fonts/fonts";
+import { useEffect, useRef } from "react";
 import { FaLocationDot } from "react-icons/fa6";
-import { MdOutlinePhone } from "react-icons/md";
-import { MdOutlineEmail } from "react-icons/md";
+import { MdOutlinePhone, MdOutlineEmail } from "react-icons/md";
+import Image from "next/image";
+import map from "./../../../public/map.png";
+import location from "./../../../public/location.gif";
 
 const ContactUs = () => {
+  const containerRef = useRef(null);
+  const subtitleRef = useRef(null);
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const spanRef = useRef(null);
+  const locationRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.fromTo(
+      [
+        subtitleRef.current,
+        titleRef.current,
+        descriptionRef.current,
+        spanRef.current,
+      ],
+      {
+        opacity: 0,
+        x: 50,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        stagger: 0.2,
+        duration: 1.5,
+        ease: "power1.out",
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top+=200 80%",
+          end: "top 20%",
+        },
+      }
+    );
+
+    gsap.fromTo(
+      locationRef.current,
+      {
+        scale: 0,
+      },
+      {
+        scale: 1,
+        ease: "power1.out",
+        delay: 0.5,
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top+=200 50%",
+          end: "top 20%",
+          // markers: true,
+        },
+      }
+    );
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.ContactUs}>
+      <div className={styles.ContactUs} ref={containerRef}>
         <div className={styles.info}>
           <div className={styles.title}>
-            <span className={poppins.className}>Contacto</span>
-            <h2 className={poppins.className}>Conversemos</h2>
-            <p className={poppins.className}>
+            <span className={poppins.className} ref={subtitleRef}>
+              Contacto
+            </span>
+            <h2 className={poppins.className} ref={titleRef}>
+              Conversemos
+            </h2>
+            <p className={poppins.className} ref={descriptionRef}>
               ¿Listo para llevar tu presencia en línea al siguiente nivel?
               ¡Hablemos de tu proyecto! Contáctanos para una consulta gratuita.
             </p>
           </div>
-          <div className={styles.infoContainer}>
+          <div className={styles.infoContainer} ref={spanRef}>
             <span className={poppins.className}>
               <FaLocationDot className={styles.icon} />
               Juan José paso 1353, Morón
@@ -35,8 +97,17 @@ const ContactUs = () => {
           </div>
         </div>
         <div className={styles.mapContainer}>
-          <div className={styles.gradient}></div>
-          <MapComponent />
+          <a
+            className={styles.gradient}
+            href="https://maps.app.goo.gl/GMFqPPhv4dxQHZzv7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span ref={locationRef}>
+              <Image src={location} alt="map" width={50} height={50} />
+            </span>
+          </a>
+          <Image src={map} alt="map" width={500} height={500} />
         </div>
       </div>
     </div>
