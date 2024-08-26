d9f7496df8e8991cb260a6b572a6a17e4b02bbff
agusasaad <agusasaad1099@hotmail.com>
2024-08-26 18:17:46 -0300

    se elimina animacion que nose usaba

--- security-and-system-page/src/app/page.js
+++ security-and-system-page/src/app/page.js
@@ -3,35 +3,20 @@ import SectionOne from "./../Components/sectionone/SectionOne";
 import HomePage from "./../Components/homePage/HomePage";
 import styles from "./page.module.css";
 import SectionThree from "@/Components/sectionthree/SectionThree";
-import React, { useEffect, useRef, useState } from "react";
-import gsap from "gsap";
+import { useState } from "react";
 import ContactUs from "@/Components/contactus/ContactUs";
 import Modal from "@/Components/modal/Modal";
 import ButtonOpenModal from "@/Components/modal/buttonModal/ButtonOpenModal";
 
 export default function Home() {
-  const bgRef = useRef(null);
   const [isModalOpen, setModalOpen] = useState(false);
-
-  useEffect(() => {
-    gsap.set([bgRef.current], {
-      opacity: 0,
-      scale: 0,
-    });
-    gsap.to([bgRef.current], {
-      opacity: 1,
-      scale: 1,
-      ease: "power1",
-      duration: 0.4,
-    });
-  }, []);
 
   return (
     <div className={styles.containerApp}>
       <ButtonOpenModal isModalOpen={isModalOpen} setModalOpen={setModalOpen} />
       <header className={styles.header}>
         <HomePage />
-        <div className={styles.bg} ref={bgRef}></div>
+        <div className={styles.bg}></div>
       </header>
       <main className={styles.main}>
         <SectionOne />
