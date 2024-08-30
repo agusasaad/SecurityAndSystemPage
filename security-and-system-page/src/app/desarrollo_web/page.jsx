c7a952cc5e64d1d3fef242892fcd48013c1af596
agusasaad <agusasaad1099@hotmail.com>
2024-08-29 23:03:54 -0300

    se agregan nuevos componentes a page de desarrollo web

--- security-and-system-page/src/app/desarrollo_web/page.jsx
+++ security-and-system-page/src/app/desarrollo_web/page.jsx
@@ -6,7 +6,10 @@ import { TbSettingsCode } from "react-icons/tb";
 import { TbDeviceMobileCode } from "react-icons/tb";
 import { BsDatabaseCheck } from "react-icons/bs";
 import { gsap } from "gsap";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Modal from "./modal_desarrollo_web/Modal";
+import cinq_capital from "./../../../public/project_1.jpg";
+import fest_club from "./../../../public/project_4.jpg";
 
 const DesarrolloWeb = () => {
   const titleRef = useRef(null);
@@ -15,6 +18,14 @@ const DesarrolloWeb = () => {
   const cardThree = useRef(null);
   const cardFour = useRef(null);
   const gradientRef = useRef(null);
+
+  const [showModal, setShowModal] = useState(false);
+  const [showComponent, setShowComponent] = useState("");
+
+  const handleModal = (component) => {
+    setShowComponent(component);
+    setShowModal(true);
+  };
 
   useEffect(() => {
     const tl = gsap.timeline();
@@ -75,11 +86,18 @@ const DesarrolloWeb = () => {
               Desarrollo de sitios web personalizados.
             </h2>
             <p className={poppins.className}>
-              Creamos sitios web únicos que reflejan tu marca y satisfacen tus
-              necesidades. Nos enfocamos en un diseño intuitivo y un rendimiento
-              optimizado para ofrecer una experiencia excepcional y alcanzar tus
-              objetivos.
+              Creamos sitios web personalizados que reflejan tu marca, enfocados
+              en un diseño intuitivo y un rendimiento optimizado para ofrecer
+              una experiencia excepcional y cumplir tus objetivos.
             </p>
+            <div className={styles.containerButton}>
+              <button
+                className={poppins.className}
+                onClick={() => handleModal("Personalizado")}
+              >
+                Ver Más
+              </button>
+            </div>
           </div>
 
           <div className={styles.personalizado} ref={cardTwo}>
@@ -100,6 +118,14 @@ const DesarrolloWeb = () => {
               la eficiencia y el rendimiento. Nuestro enfoque garantiza código
               limpio, mantenible y escalable para proyectos.
             </p>
+            <div className={styles.containerButton}>
+              <button
+                className={poppins.className}
+                onClick={() => handleModal("ProgramacionCodificacion")}
+              >
+                Ver Más
+              </button>
+            </div>
           </div>
 
           <div className={styles.personalizado} ref={cardThree}>
@@ -118,6 +144,14 @@ const DesarrolloWeb = () => {
               dispositivo, garantizando una experiencia de usuario fluida y
               accesible desde smartphones, tablets y más.
             </p>
+            <div className={styles.containerButton}>
+              <button
+                className={poppins.className}
+                onClick={() => handleModal("DiseñoResponsiveMovile")}
+              >
+                Ver Más
+              </button>
+            </div>
           </div>
 
           <div className={styles.personalizado} ref={cardFour}>
@@ -134,14 +168,131 @@ const DesarrolloWeb = () => {
               Integración de base de datos y gestión.
             </h2>
             <p className={poppins.className}>
-              Implementamos y gestionamos bases de datos robustas que aseguran
-              la eficiencia y seguridad de tu información, optimizando el
-              rendimiento y facilitando el acceso a los datos críticos de tu
+              Implementamos y gestionamos bases de datos seguras y optimizadas
+              para garantizar el acceso eficiente a la información crítica de tu
               negocio.
             </p>
+            <div className={styles.containerButton}>
+              <button
+                className={poppins.className}
+                onClick={() => handleModal("BaseDeDatos")}
+              >
+                Ver Más
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.sectionTwo}>
+          <div className={styles.cardProyect}>
+            <img
+              src={cinq_capital.src}
+              alt=""
+              width={"600px"}
+              height={"auto"}
+            />
+            <div className={styles.containerInfo}>
+              <h4 className={poppins.className}>Cinq Capital</h4>
+              <p className={poppins.className}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Expedita quas, rerum officiis optio, vero accusantium sequi
+                itaque atque similique dignissimos harum corrupti ad doloribus
+                labore voluptas. Architecto eveniet animi aspernatur.
+              </p>
+              <div className={styles.containerService}>
+                <span
+                  style={{
+                    background: "rgba(0, 189, 87, 0.1)",
+                    color: "rgb(0, 189, 87)",
+                  }}
+                >
+                  <PiCodeBold style={{ fontSize: "15px" }} />
+                  Personalización
+                </span>
+                <span
+                  style={{
+                    background: "#9162c025",
+                    color: "#c084fd",
+                  }}
+                >
+                  <TbSettingsCode style={{ fontSize: "15px" }} />
+                  Eficiencia
+                </span>
+                <span
+                  style={{
+                    background: "#3b88e925",
+                    color: "#4e9eff",
+                  }}
+                >
+                  <TbDeviceMobileCode style={{ fontSize: "15px" }} />
+                  Adaptabilidad
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardProyect}>
+            <img src={fest_club.src} alt="" width={"600px"} height={"auto"} />
+            <div className={styles.containerInfo}>
+              <h4 className={poppins.className}>Fest Club</h4>
+              <p className={poppins.className}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Expedita quas, rerum officiis optio, vero accusantium sequi
+                itaque atque similique dignissimos harum corrupti ad doloribus
+                labore voluptas. Architecto eveniet animi aspernatur.
+              </p>
+              <div className={styles.containerService}>
+                <span
+                  style={{
+                    background: "rgba(0, 189, 87, 0.1)",
+                    color: "rgb(0, 189, 87)",
+                  }}
+                >
+                  <PiCodeBold style={{ fontSize: "15px" }} />
+                  Personalización
+                </span>
+                <span
+                  style={{
+                    background: "#9162c025",
+                    color: "#c084fd",
+                  }}
+                >
+                  <TbSettingsCode style={{ fontSize: "15px" }} />
+                  Eficiencia
+                </span>
+                <span
+                  style={{
+                    background: "#3b88e925",
+                    color: "#4e9eff",
+                  }}
+                >
+                  <TbDeviceMobileCode style={{ fontSize: "15px" }} />
+                  Adaptabilidad
+                </span>
+                <span
+                  style={{
+                    background: "#d1772425",
+                    color: "#ff9a3c",
+                  }}
+                >
+                  <BsDatabaseCheck style={{ fontSize: "15px" }} />
+                  Gestión
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.containerButtonSectionTwo}>
+            <button className={poppins.className}>
+              Ver Todos los Proyectos
+            </button>
           </div>
         </section>
       </div>
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        showComponent={showComponent}
+      />
     </div>
   );
 };
