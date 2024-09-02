d38307d1a5c7102724fcd3b948d399abdfd10c43
agusasaad <agusasaad1099@hotmail.com>
2024-09-02 18:05:52 -0300

    se agrega seccion proyectos

---
+++ security-and-system-page/src/app/web_proyectos/page.jsx
@@ -0,0 +1,79 @@
+import { poppins } from "@/Fonts/fonts";
+import styles from "./page.module.css";
+import web1 from "./../../../public/cinq_capital.png";
+import web2 from "./../../../public/fest_club.png";
+import web3 from "./../../../public/fiestas.png";
+import web4 from "./../../../public/sdp.png";
+import web5 from "./../../../public/que_dj.png";
+import web6 from "./../../../public/net_expertos.png";
+import web7 from "./../../../public/tu_voz_suma.png";
+
+const page = () => {
+  const imageCarrucel = [web1, web2, web5, web3, web6, web4, web7];
+
+  const copy = [...imageCarrucel, ...imageCarrucel];
+
+  return (
+    <div className={`${styles.container} ${poppins.className}`}>
+      <div className={styles.containerTitle}>
+        <span>Proyectos Realizados</span>
+        <h1>Explora Nuestra variedad de Proyectos.</h1>
+      </div>
+      <div className={styles.containerCards}>
+        <div className={styles.containerCarrucel}>
+          {copy.map((element, index) => (
+            <div key={index} className={styles.card_web}>
+              <img
+                src={element.src}
+                width={"600px"}
+                height={"auto"}
+                alt="imagen proyecto"
+              />
+
+              <div className={styles.buttons}>
+                <button
+                  className={`${poppins.className} ${styles.button_blue}`}
+                >
+                  Detalles
+                </button>
+                <button
+                  className={`${poppins.className} ${styles.button_black}`}
+                >
+                  Link
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.containerCarrucel_mobile}>
+          {imageCarrucel.map((element, index) => (
+            <div key={index} className={styles.card_mobile}>
+              <img
+                src={element.src}
+                width={"100%"}
+                height={"auto"}
+                alt="imagen proyecto"
+              />
+
+              <div className={styles.buttons_mobile}>
+                <button
+                  className={`${poppins.className} ${styles.button_blue}`}
+                >
+                  Detalles
+                </button>
+                <button
+                  className={`${poppins.className} ${styles.button_black}`}
+                >
+                  Link
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
