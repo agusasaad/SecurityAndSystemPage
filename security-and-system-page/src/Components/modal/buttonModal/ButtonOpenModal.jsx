efa5c41b2f86b7e7cd6c885fe664936b0613f491
agusasaad <agusasaad1099@hotmail.com>
2024-09-03 12:17:42 -0300

    cambios en el boton de modal

--- security-and-system-page/src/Components/modal/buttonModal/ButtonOpenModal.jsx
+++ security-and-system-page/src/Components/modal/buttonModal/ButtonOpenModal.jsx
@@ -1,15 +1,39 @@
 "use client";
 import styles from "./ButtonOpenModal.module.css";
 import { poppins } from "@/Fonts/fonts";
-import { IoMdChatbubbles } from "react-icons/io";
+import gif_button from "./../../../../public/gif_button.gif";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 const ButtonOpenModal = ({ isModalOpen, setModalOpen }) => {
+  const buttonRef = useRef(null);
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.fromTo(
+      buttonRef.current,
+      {
+        width: "100%",
+      },
+      {
+        width: "150px",
+        ease: "linear",
+        duration: 0.2,
+        scrollTrigger: {
+          trigger: buttonRef.current,
+          start: "top 60%",
+          end: "top 20%",
+        },
+      }
+    );
+  }, []);
   return (
-    <div className={styles.containerButton}>
+    <div className={styles.containerButton} ref={buttonRef}>
       <button
         className={poppins.className}
         onClick={() => setModalOpen(!isModalOpen)}
       >
-        <IoMdChatbubbles />
+        <img src={gif_button.src} alt="" />
       </button>
     </div>
   );
