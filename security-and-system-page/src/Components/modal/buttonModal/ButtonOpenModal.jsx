f83dde8c2d3808900e1ba333105b107e72bbf91f
agusasaad <agusasaad1099@hotmail.com>
2024-09-03 21:51:36 -0300

    animation button

--- security-and-system-page/src/Components/modal/buttonModal/ButtonOpenModal.jsx
+++ security-and-system-page/src/Components/modal/buttonModal/ButtonOpenModal.jsx
@@ -1,17 +1,38 @@
 "use client";
 import styles from "./ButtonOpenModal.module.css";
 import { poppins } from "@/Fonts/fonts";
+import { useEffect, useRef } from "react";
 import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
+import gsap from "gsap";
 
 const ButtonOpenModal = ({ isModalOpen, setModalOpen }) => {
+  const circleRef = useRef(null);
+  const animationRef = useRef(null);
+
+  useEffect(() => {
+    animationRef.current = gsap.to(circleRef.current, {
+      scale: 1.7,
+      opacity: 0,
+      ease: "power1",
+      repeat: -1,
+      duration: 1.3,
+      delay: 3,
+    });
+  }, []);
+
+  const handleClick = () => {
+    if (animationRef.current) {
+      animationRef.current.kill();
+    }
+    setModalOpen(!isModalOpen);
+  };
+
   return (
     <div className={styles.containerButton}>
-      <button
-        className={poppins.className}
-        onClick={() => setModalOpen(!isModalOpen)}
-      >
+      <button className={poppins.className} onClick={handleClick}>
         <IoChatbubbleEllipsesSharp />
       </button>
+      <div className={styles.circle} ref={circleRef}></div>
     </div>
   );
 };
