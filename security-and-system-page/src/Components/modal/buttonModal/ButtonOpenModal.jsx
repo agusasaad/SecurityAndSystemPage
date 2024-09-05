27f422a15f7f3a0319b9c01bcff35d6372de2e9c
agusasaad <agusasaad1099@hotmail.com>
2024-09-04 21:44:06 -0300

    nuevo icono en boton de modal

--- security-and-system-page/src/Components/modal/buttonModal/ButtonOpenModal.jsx
+++ security-and-system-page/src/Components/modal/buttonModal/ButtonOpenModal.jsx
@@ -4,35 +4,18 @@ import { poppins } from "@/Fonts/fonts";
 import { useEffect, useRef } from "react";
 import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
 import gsap from "gsap";
+import gifBlubble from "./../../../../public/mecanografia.gif";
 
 const ButtonOpenModal = ({ isModalOpen, setModalOpen }) => {
-  const circleRef = useRef(null);
-  const animationRef = useRef(null);
-
-  useEffect(() => {
-    animationRef.current = gsap.to(circleRef.current, {
-      scale: 1.7,
-      opacity: 0,
-      ease: "power1",
-      repeat: -1,
-      duration: 1.3,
-      delay: 3,
-    });
-  }, []);
-
   const handleClick = () => {
-    if (animationRef.current) {
-      animationRef.current.kill();
-    }
     setModalOpen(!isModalOpen);
   };
 
   return (
     <div className={styles.containerButton}>
       <button className={poppins.className} onClick={handleClick}>
-        <IoChatbubbleEllipsesSharp />
+        <img src={gifBlubble.src} alt="" width={60} height={60} />
       </button>
-      <div className={styles.circle} ref={circleRef}></div>
     </div>
   );
 };
