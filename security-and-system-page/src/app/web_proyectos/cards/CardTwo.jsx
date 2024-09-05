8e73858ea68e00adfa219ef7b80bbade87facc5d
agusasaad <agusasaad1099@hotmail.com>
2024-09-05 20:36:30 -0300

    nueva card two

--- security-and-system-page/src/app/web_proyectos/cards/CardTwo.jsx
+++ security-and-system-page/src/app/web_proyectos/cards/CardTwo.jsx
@@ -1,13 +1,12 @@
 "use client";
 import { poppins } from "@/Fonts/fonts";
 import styles from "./CardTwo.module.css";
-import web1 from "./../../../../public/proyectos/que_dj_optimizada.jpg";
-import web4 from "./../../../../public/proyectos/net_expertos_optimizada.jpg";
-import web3 from "./../../../../public/proyectos/tu_voz_suma_optimizada.jpg";
-import web2 from "./../../../../public/proyectos/dni_optimizado.jpg";
+import web1 from "./../../../../public/proyectos/net_expertos_optimizada.jpg";
+import web2 from "./../../../../public/proyectos/tu_voz_suma_optimizada.jpg";
+import web3 from "./../../../../public/proyectos/dni_optimizado.jpg";
+import web4 from "./../../../../public/proyectos/que_dj_optimizada.jpg";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const CardTwo = () => {
   const subtitleRef = useRef(null);
@@ -18,23 +17,39 @@ const CardTwo = () => {
   const card_three = useRef(null);
   const card_four = useRef(null);
 
+  const initialCards = [
+    { id: 1, title: "Net Expertos", imageSrc: web1.src, ref: card_one },
+    { id: 2, title: "Tu voz Suma", imageSrc: web2.src, ref: card_two },
+    { id: 3, title: "DNI Logistica", imageSrc: web3.src, ref: card_three },
+    { id: 4, title: "Que DJ", imageSrc: web4.src, ref: card_four },
+  ];
+
+  const [cards, setCards] = useState(initialCards);
+  const [isMobile, setIsMobile] = useState(false); // Nuevo estado para móvil
+
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Detecta si la pantalla es móvil
+    };
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: containerCard.current,
-        start: "top-=300 90%",
-        end: "bottom 20%",
-      },
-    });
+    // Llama a la función para inicializar al montar
+    handleResize();
 
-    // Animación inicial de entrada
-    if (window.innerWidth > 750) {
+    // Listener para detectar cambios en el tamaño de la ventana
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup del listener cuando se desmonta el componente
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+    if (window.innerWidth > 768) {
       tl.fromTo(
         [
           subtitleRef.current,
           titleRef.current,
+          containerCard.current,
           card_one.current,
           card_two.current,
           card_three.current,
@@ -48,8 +63,53 @@ const CardTwo = () => {
           duration: 0.8,
           stagger: 0.1,
           ease: "power3.out",
+          delay: 0.5,
         }
       );
+      tl.to(card_one.current, {
+        rotation: -45,
+        x: -170,
+        y: 50,
+        boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
+        duration: 0.5,
+        ease: "power3.out",
+      })
+        .to(
+          card_two.current,
+          {
+            rotation: -30,
+            x: -120,
+            y: 23,
+            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
+            duration: 0.5,
+            ease: "power3.out",
+          },
+          "<"
+        )
+        .to(
+          card_three.current,
+          {
+            rotation: -15,
+            x: -60,
+            y: 5,
+            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
+            duration: 0.5,
+            ease: "power3.out",
+          },
+          "<"
+        )
+        .to(
+          card_four.current,
+          {
+            rotation: 0,
+            x: 0,
+            y: 0,
+            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
+            duration: 0.5,
+            ease: "power3.out",
+          },
+          "<"
+        );
     } else {
       tl.fromTo(
         [subtitleRef.current, titleRef.current, containerCard.current],
@@ -64,72 +124,21 @@ const CardTwo = () => {
           delay: 0.5,
         }
       );
-    }
-
-    // Animación de apertura como un mazo de cartas
-    if (window.innerWidth > 750) {
       tl.to(card_one.current, {
-        rotation: -15,
-        x: -80,
-        y: -10,
-        boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
-        pointerEvents: "none",
-        duration: 0.5,
-        ease: "power3.out",
-      })
-        .to(
-          card_two.current,
-          {
-            rotation: -5,
-            x: -25,
-            y: -15,
-            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
-            pointerEvents: "none",
-            duration: 0.5,
-            ease: "power3.out",
-          },
-          "<"
-        )
-        .to(
-          card_three.current,
-          {
-            rotation: 10,
-            x: 25,
-            y: -5,
-            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
-            pointerEvents: "none",
-            duration: 0.5,
-            ease: "power3.out",
-          },
-          "<"
-        )
-        .to(
-          card_four.current,
-          {
-            rotation: 25,
-            x: 72,
-            y: 15,
-            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
-            pointerEvents: "none",
-            duration: 0.5,
-            ease: "power3.out",
-          },
-          "<"
-        );
-    } else {
-      tl.to(card_one.current, {
+        rotation: 0,
+        x: 0,
         y: 0,
-        pointerEvents: "none",
-        boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.5)",
+        boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
         duration: 0.5,
         ease: "power3.out",
       })
         .to(
           card_two.current,
           {
+            rotation: 0,
+            x: 0,
             y: 50,
-            pointerEvents: "none",
-            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.5)",
+            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
             duration: 0.5,
             ease: "power3.out",
           },
@@ -138,9 +147,10 @@ const CardTwo = () => {
         .to(
           card_three.current,
           {
+            rotation: 0,
+            x: 0,
             y: 100,
-            pointerEvents: "none",
-            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.5)",
+            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
             duration: 0.5,
             ease: "power3.out",
           },
@@ -149,9 +159,10 @@ const CardTwo = () => {
         .to(
           card_four.current,
           {
+            rotation: 0,
+            x: 0,
             y: 150,
-            pointerEvents: "none",
-            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.5)",
+            boxShadow: "0px 0px 19px 0px rgba(0, 0, 0, 0.808)",
             duration: 0.5,
             ease: "power3.out",
           },
@@ -160,69 +171,49 @@ const CardTwo = () => {
     }
   }, []);
 
-  const resetCards = () => {
-    if (window.innerWidth > 750) {
-      gsap.to(
-        [
-          card_one.current,
-          card_two.current,
-          card_three.current,
-          card_four.current,
-        ],
-        {
-          rotation: 0,
-          x: 0,
-          y: 0,
-          pointerEvents: "auto",
-          display: "flex",
-          alignItems: "center",
-          duration: 0.5,
-          ease: "power3.out",
-        }
-      );
+  const moveCardToBack = (cardId) => {
+    const selectedCard = cards.find((card) => card.id === cardId);
+    const reorderedCards = [
+      ...cards.filter((card) => card.id !== cardId),
+      selectedCard,
+    ];
+    setCards(reorderedCards);
 
-      gsap.to([containerCard.current], {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "15px",
-        marginTop: "30px",
-        flexWrap: "wrap",
-        duration: 0.3,
+    reorderedCards.forEach((card, index) => {
+      const { x, y, rotation } = getCardTransform(index, isMobile); // Pasar isMobile
+      gsap.to(card.ref.current, {
+        rotation,
+        x,
+        y,
+        duration: 0.4,
         ease: "power1",
-        delay: 0.5,
       });
-    } else {
-      gsap.to([containerCard.current], {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        flexWrap: "wrap",
-        gap: "15px",
-        paddingBottom: "50px",
-        duration: 0.3,
-        ease: "power1",
-        delay: 0.5,
-      });
-      gsap.to(
-        [
-          card_one.current,
-          card_two.current,
-          card_three.current,
-          card_four.current,
-        ],
-        {
-          y: 0,
-          display: "flex",
-          alignItems: "center",
-          pointerEvents: "auto",
-          duration: 0.5,
-          ease: "power3.out",
-        }
-      );
-    }
+
+      card.ref.current.style.zIndex = index + 1; // Mantiene el z-index correcto
+    });
+  };
+
+  const getCardTransform = (index, isMobile) => {
+    // Transformaciones para escritorio
+    const desktopTransformations = [
+      { rotation: -45, x: -170, y: 50 }, // Card 1
+      { rotation: -30, x: -120, y: 23 }, // Card 2
+      { rotation: -15, x: -60, y: 5 }, // Card 3
+      { rotation: 0, x: 0, y: 0 }, // Card 4
+    ];
+
+    // Transformaciones para móvil
+    const mobileTransformations = [
+      { rotation: 0, x: 0, y: 0 }, // Card 1
+      { rotation: 0, x: 0, y: 50 }, // Card 2
+      { rotation: 0, x: 0, y: 100 }, // Card 3
+      { rotation: 0, x: 0, y: 150 }, // Card 4
+    ];
+
+    // Retorna las transformaciones dependiendo si es móvil o no
+    return isMobile
+      ? mobileTransformations[index]
+      : desktopTransformations[index];
   };
 
   return (
@@ -235,39 +226,27 @@ const CardTwo = () => {
           Proyectos Mobile
         </h1>
       </div>
-      <div
-        className={styles.containerCards}
-        ref={containerCard}
-        onClick={resetCards}
-      >
-        <div className={styles.card} ref={card_one}>
-          <img src={web1.src} alt="" />
-          <div className={styles.title}>
-            <h5 className={poppins.className}>Que DJ</h5>
-            <button>Detalles</button>
+      <div className={styles.containerCards} ref={containerCard}>
+        {cards.map((card) => (
+          <div
+            key={card.id}
+            className={styles.card}
+            ref={card.ref}
+            onClick={() => moveCardToBack(card.id)}
+          >
+            <img src={card.imageSrc} alt={card.title} />
+            <div className={styles.title}>
+              <h5 className={poppins.className}>{card.title}</h5>
+              <button
+                onClick={() => {
+                  console.log(card.id);
+                }}
+              >
+                Detalles
+              </button>
+            </div>
           </div>
-        </div>
-        <div className={styles.card} ref={card_two}>
-          <img src={web2.src} alt="" />
-          <div className={styles.title}>
-            <h5 className={poppins.className}>DNI Logistica</h5>
-            <button>Detalles</button>
-          </div>
-        </div>
-        <div className={styles.card} ref={card_three}>
-          <img src={web3.src} alt="" />
-          <div className={styles.title}>
-            <h5 className={poppins.className}>Tu Voz Suma</h5>
-            <button>Detalles</button>
-          </div>
-        </div>
-        <div className={styles.card} ref={card_four}>
-          <img src={web4.src} alt="" />
-          <div className={styles.title}>
-            <h5 className={poppins.className}>Net Expertos</h5>
-            <button>Detalles</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
