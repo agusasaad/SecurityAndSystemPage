c9391e87711f6fdcfaf9681f5bcd32c61c1bf60a
agusasaad <agusasaad1099@hotmail.com>
2024-09-18 15:25:35 -0300

    details

--- security-and-system-page/src/app/web_proyectos/[id]/Info.js
+++ security-and-system-page/src/app/web_proyectos/[id]/Info.js
@@ -1,90 +1,90 @@
-import cinq_capital_mockup_1 from "./../../../../public/proyectos/detail_Cinq_capital/mockup_1.jpeg";
-import cinq_capital_mockup_2 from "./../../../../public/proyectos/detail_Cinq_capital/mockup_2.png";
-import cinq_capital_mockup_3 from "./../../../../public/proyectos/detail_Cinq_capital/mockup_3.png";
-import festClub_img from "./../../../../public/project_4.jpg";
-import salon_plaza_banner from "./../../../../public/proyectos/fiestas_optimizada.jpg";
-import salon_plaza_img from "./../../../../public/project_3.jpg";
-import sdp_revestimientos_banner from "./../../../../public/proyectos/sdp_optimizada.jpg";
-import sdp_revestimientos_img from "./../../../../public/project_5.jpg";
-import net_expertos_banner from "./../../../../public/proyectos/net_expertos_optimizada.jpg";
-import net_expertos_img from "./../../../../public/proyectos/net_expertos_optimizada.jpg";
-import tu_voz_suma_banner from "./../../../../public/proyectos/tu_voz_suma_optimizada.jpg";
-import tu_voz_suma_img from "./../../../../public/proyectos/tu_voz_suma_optimizada.jpg";
-import dni_logistica_banner from "./../../../../public/proyectos/dni_optimizado.jpg";
-import dni_logistica_img from "./../../../../public/proyectos/dni_optimizado.jpg";
-import que_dj_banner from "./../../../../public/proyectos/que_dj_optimizada.jpg";
-import que_dj_img from "./../../../../public/proyectos/que_dj_optimizada.jpg";
-
+import cinq_capital_mockup_1 from './../../../../public/proyectos/detail_Cinq_capital/mockup_1.jpeg'
+import cinq_capital_mockup_2 from './../../../../public/proyectos/detail_Cinq_capital/mockup_2.png'
+import cinq_capital_mockup_3 from './../../../../public/proyectos/detail_Cinq_capital/mockup_3.png'
 export const desarrolloWeb = [
   {
-    id: "1",
-    title: "Cinq Capital",
-    banner: "Invierte y maximiza tu capital.",
+    id: '1',
+    title: 'Cinq Capital',
+    banner: 'Invierte y maximiza tu capital.',
     infoText:
-      "Cinq Capital es una plataforma de inversiones diseñada para ofrecer soluciones financieras avanzadas. Facilita el acceso a oportunidades de inversión, brindando herramientas y recursos para gestionar y optimizar tu cartera de forma eficaz.",
+      'Cinq Capital es una plataforma de inversiones diseñada para ofrecer soluciones financieras avanzadas. Facilita el acceso a oportunidades de inversión, brindando herramientas y recursos para gestionar y optimizar tu cartera de forma eficaz.',
     img: cinq_capital_mockup_1.src,
     img_2: cinq_capital_mockup_2.src,
     img_3: cinq_capital_mockup_3.src,
   },
 
   {
-    id: "2",
-    title: "Fest Club",
-    banner: "Expande tu universo",
+    id: '2',
+    title: 'Fest Club',
+    banner: 'Expande tu universo',
     infoText:
-      "Fest Club es una plataforma digital para la compra de entradas a eventos en la discoteca. Permite a los usuarios explorar eventos, seleccionar fechas y adquirir boletos de forma rápida y sencilla, proporcionando una experiencia de compra eficiente y sin complicaciones.",
-    img: festClub_img.src,
+      'Fest Club es una plataforma digital para la compra de entradas a eventos en la discoteca. Permite a los usuarios explorar eventos, seleccionar fechas y adquirir boletos de forma rápida y sencilla, proporcionando una experiencia de compra eficiente y sin complicaciones.',
+    img: cinq_capital_mockup_1.src,
+    img_2: cinq_capital_mockup_2.src,
+    img_3: cinq_capital_mockup_3.src,
   },
 
   {
-    id: "3",
-    title: "Salon Plaza",
+    id: '3',
+    title: 'Salon Plaza',
     infoText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.",
-    img: salon_plaza_img.src,
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.',
+    img: cinq_capital_mockup_1.src,
+    img_2: cinq_capital_mockup_2.src,
+    img_3: cinq_capital_mockup_3.src,
   },
 
   {
-    id: "4",
-    title: "SDP Revestimientos",
-    banner: "Expande tu universo",
+    id: '4',
+    title: 'SDP Revestimientos',
+    banner: 'Expande tu universo',
     infoText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.",
-    img: sdp_revestimientos_img.src,
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.',
+    img: cinq_capital_mockup_1.src,
+    img_2: cinq_capital_mockup_2.src,
+    img_3: cinq_capital_mockup_3.src,
   },
   {
-    id: "5",
-    title: "Net Expertos",
-    banner: "Expande tu universo",
+    id: '5',
+    title: 'Net Expertos',
+    banner: 'Expande tu universo',
     infoText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.",
-    img: net_expertos_img.src,
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.',
+    img: cinq_capital_mockup_1.src,
+    img_2: cinq_capital_mockup_2.src,
+    img_3: cinq_capital_mockup_3.src,
   },
 
   {
-    id: "6",
-    title: "Tu voz Suma",
-    banner: "Expande tu universo",
+    id: '6',
+    title: 'Tu voz Suma',
+    banner: 'Expande tu universo',
     infoText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.",
-    img: tu_voz_suma_img.src,
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.',
+    img: cinq_capital_mockup_1.src,
+    img_2: cinq_capital_mockup_2.src,
+    img_3: cinq_capital_mockup_3.src,
   },
 
   {
-    id: "7",
-    title: "DNI Logistica",
-    banner: "Expande tu universo",
+    id: '7',
+    title: 'DNI Logistica',
+    banner: 'Expande tu universo',
     infoText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.",
-    img: dni_logistica_img.src,
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.',
+    img: cinq_capital_mockup_1.src,
+    img_2: cinq_capital_mockup_2.src,
+    img_3: cinq_capital_mockup_3.src,
   },
 
   {
-    id: "8",
-    title: "Que DJ",
-    banner: "Expande tu universo",
+    id: '8',
+    title: 'Que DJ',
+    banner: 'Expande tu universo',
     infoText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.",
-    img: que_dj_img.src,
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit.laboriosam quod debitis vitae repellendus eum. Fugiat laborumvoluptatibus architecto, labore distinctio et eos officia adipiscitempore exercitationem voluptates cupiditate in.',
+    img: cinq_capital_mockup_1.src,
+    img_2: cinq_capital_mockup_2.src,
+    img_3: cinq_capital_mockup_3.src,
   },
-];
+]
--- security-and-system-page/src/app/web_proyectos/[id]/page.jsx
+++ security-and-system-page/src/app/web_proyectos/[id]/page.jsx
@@ -1,45 +1,97 @@
-"use client";
-import styles from "./page.module.css";
-import { desarrolloWeb } from "./Info";
-import { useParams, useRouter } from "next/navigation";
-import { IoArrowBack, IoArrowForward } from "react-icons/io5";
-import { poppins } from "@/Fonts/fonts";
-import Link from "next/link";
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
+'use client'
+import styles from './page.module.css'
+import { desarrolloWeb } from './Info'
+import { useParams, useRouter } from 'next/navigation'
+import { IoArrowBack, IoArrowForward } from 'react-icons/io5'
+import { IoMdArrowDropleft, IoMdArrowDropright } from 'react-icons/io'
+import { poppins } from '@/Fonts/fonts'
+import { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
 
 const Detail = () => {
-  const titleRef = useRef(null);
-  const parrafoRef = useRef(null);
-  const imgRef = useRef(null);
-  const { id } = useParams();
-  const router = useRouter();
+  const titleRef = useRef(null)
+  const parrafoRef = useRef(null)
+  const imgRef = useRef(null)
+  const { id } = useParams()
+  const router = useRouter()
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
   useEffect(() => {
     gsap.set([titleRef.current, parrafoRef.current, imgRef.current], {
       y: 100,
       opacity: 0,
-      visibility: "hidden",
-    });
+      visibility: 'hidden',
+    })
     gsap.to([titleRef.current, parrafoRef.current, imgRef.current], {
       opacity: 1,
       y: 0,
       duration: 0.7,
-      ease: "power1",
+      ease: 'power1',
       stagger: 0.2,
-      visibility: "visible",
-    });
-  }, []);
+      visibility: 'visible',
+    })
+  }, [])
 
   const handleBack = (id) => {
-    router.push(`/web_proyectos/${Number(id) - 1}`);
-  };
+    router.push(`/web_proyectos/${Number(id) - 1}`)
+  }
 
   const handleNext = (id) => {
-    router.push(`/web_proyectos/${Number(id) + 1}`);
-  };
+    router.push(`/web_proyectos/${Number(id) + 1}`)
+  }
 
-  const showProject = desarrolloWeb.find((proyecto) => proyecto.id === id);
+  const handleImage = (direction) => {
+    const containerWidth = document.querySelector(
+      `.${styles.images} img`
+    ).offsetWidth
+
+    const totalImages = document.querySelectorAll(
+      `.${styles.images} img`
+    ).length
+    let newIndex = currentImageIndex
+
+    if (direction === 'next') {
+      if (currentImageIndex === totalImages - 1) {
+        if (Number(id) !== 8) {
+          handleNext(Number(id))
+        } else {
+          router.push('/web_proyectos/1')
+        }
+      } else {
+        newIndex = currentImageIndex + 1
+      }
+    } else if (direction === 'back') {
+      if (currentImageIndex === 0) {
+        if (Number(id) !== 1) {
+          handleBack(Number(id))
+        } else {
+          router.push('/web_proyectos/8')
+        }
+      } else {
+        newIndex = currentImageIndex - 1
+      }
+    }
+
+    setCurrentImageIndex(newIndex)
+
+    if (window.innerWidth > 1450) {
+      gsap.to('#images', {
+        x: -newIndex * containerWidth + 50,
+        duration: 0.8,
+        ease: 'power1',
+      })
+    } else {
+      gsap.to('#images', {
+        x: -newIndex * containerWidth,
+        duration: 0.8,
+        ease: 'power1',
+      })
+    }
+  }
+
+  const showProject = desarrolloWeb.find((proyecto) => proyecto.id === id)
+
   return (
     <div className={styles.container}>
       <div className={styles.detailContainer}>
@@ -50,42 +102,52 @@ const Detail = () => {
             <button className={poppins.className}>Visitar web</button>
           </div>
           <div className={styles.parrafo} ref={parrafoRef}>
-            <h5 className={poppins.className}>SOBRE EL PROYECTO</h5>
+            <h5 className={poppins.className}>Sobre el proyecto:</h5>
             <p className={poppins.className}>{showProject?.infoText}</p>
           </div>
         </div>
         <div className={styles.containerImg} ref={imgRef}>
-          <img src={showProject?.img_3 || showProject?.img} alt="" />
-          <img src={showProject?.img_2 || showProject?.img} alt="" />
-          <img src={showProject?.img} alt="" />
+          <div className={styles.buttons}>
+            <button onClick={() => handleImage('back')}>
+              <IoMdArrowDropleft className={styles.icon} />
+            </button>
+            <button onClick={() => handleImage('next')}>
+              <IoMdArrowDropright className={styles.icon} />
+            </button>
+          </div>
+          <div className={styles.images} id="images">
+            <img src={showProject?.img_3 || showProject?.img} alt="" />
+            <img src={showProject?.img_2 || showProject?.img} alt="" />
+            <img src={showProject?.img} alt="" />
+          </div>
         </div>
       </div>
       <div className={styles.controlButtons}>
         <button
           className={poppins.className}
-          onClick={() => handleBack(id)}
+          onClick={() => handleBack(Number(id))}
           disabled={Number(id) === 1}
           style={{
-            color: Number(id) === 1 ? "rgb(177, 177, 177)" : "white",
-            cursor: Number(id) === 1 ? "default" : "pointer",
+            color: Number(id) === 1 ? 'rgb(177, 177, 177)' : 'white',
+            cursor: Number(id) === 1 ? 'default' : 'pointer',
           }}
         >
           <IoArrowBack /> Atras
         </button>
         <button
           className={poppins.className}
-          onClick={() => handleNext(id)}
+          onClick={() => handleNext(Number(id))}
           disabled={Number(id) === 8}
           style={{
-            color: Number(id) === 8 ? "rgb(177, 177, 177)" : "white",
-            cursor: Number(id) === 8 ? "default" : "pointer",
+            color: Number(id) === 8 ? 'rgb(177, 177, 177)' : 'white',
+            cursor: Number(id) === 8 ? 'default' : 'pointer',
           }}
         >
           Siguiente <IoArrowForward />
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Detail;
+export default Detail
