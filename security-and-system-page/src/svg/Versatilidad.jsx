975f60f25176cedfe4ca771de767973668722134
agusasaad <agusasaad1099@hotmail.com>
2024-09-20 12:04:14 -0300

    Se crean nuevos svg's

---
+++ security-and-system-page/src/svg/Versatilidad.jsx
@@ -0,0 +1,29 @@
+const Versatilidad = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ fontSize: 15 }}
+    >
+      <path d="M12 21h-4a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v6.5" />
+      <path d="M11 4h2" />
+      <path d="M12 17v.01" />
+      <path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+      <path d="M19.001 15.5v1.5" />
+      <path d="M19.001 21v1.5" />
+      <path d="M22.032 17.25l-1.299 .75" />
+      <path d="M17.27 20l-1.3 .75" />
+      <path d="M15.97 17.25l1.3 .75" />
+      <path d="M20.733 20l1.3 .75" />
+    </svg>
+  )
+}
+
+export default Versatilidad
